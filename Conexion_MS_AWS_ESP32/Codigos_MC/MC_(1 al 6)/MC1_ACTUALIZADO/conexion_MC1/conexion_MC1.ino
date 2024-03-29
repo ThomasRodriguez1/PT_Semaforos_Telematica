9c6acdbe73234f0a8ee6edab0e3fc6f686ed9f1a
lety864 <lbermudezr1500@alumno.ipn.mx>
2024-03-29 16:32:38 -0600

    Update conexion_MC1.ino

--- Conexion_MS_AWS_ESP32/Codigos_MC/MC_(1 al 6)/MC1_ACTUALIZADO/conexion_MC1/conexion_MC1.ino
+++ Conexion_MS_AWS_ESP32/Codigos_MC/MC_(1 al 6)/MC1_ACTUALIZADO/conexion_MC1/conexion_MC1.ino
@@ -8,7 +8,7 @@
 
 //SEMAFOROS
 //Variables del programa en general
-int NuevoCiclo = 12000, nuevoValor,i = 0;
+int NuevoCiclo = 120000, nuevoValor,i = 0;
 unsigned long t_comprobador=0;
 boolean nuevoCiclo=false;
 static bool cambio=false;
@@ -32,7 +32,7 @@ float relacionAmarillo_V1=0;
 float relacionParpadeo_V1=0;
 int contador_parpadeo=0;
 int stuck_contador=9;
-//Orden Vehicular Verde->Amarillo->Rojo
+//Orden Vehicular Verde->Amarillo->Rojo (Verificar que sea correcto)
 int SV1[3]={27,14,13};
 
 
@@ -46,7 +46,9 @@ boolean initparalelo=true;
 Pausa-->      1
 Reinicio-->   2
 */
-
+//Agregadas para manejar la confirmación 28/03/2024
+bool confirmador=false;
+bool stuckConfirmador=true;
 
 //VARIABLES PARA LA CONEXION AWS
 WiFiClientSecure net = WiFiClientSecure();
@@ -214,12 +216,28 @@ void messageHandler(String &topic, String &payload)
   }
 }
 
+
+//Agregar que si recibe confirmación continue con el cambio en caso contrario su comportamiento cambia al por defecto
+/*
+El sistema puede modificarse hasta los 22.25 s de hacer iniciado
+Se configurará para que espere hasta los 22 segundos, si no recibe confirmación cambiará su tiempo al por defecto 
+*/
+
+
 void handlerACKs( JsonObject &doc){
     // Procesa el mensaje recibido en Topic1
   Serial.println("Procesando mensaje para Topic1");
   // Ejemplo: Imprimir un valor específico del JSON
 String status = doc["status"].as<String>();  // Esto asegura la conversión correcta
   Serial.println(status);
+  
+  //Podria prescindirse del condicional, pues si recibe un mensaje a este topic, es que el MPD confirma de haber recibido los mensajes de los MC's
+  if(status.equals("confirmado")){
+    confirmador=true;
+    Serial.println("Se recibió confirmación");
+  }
+  
+ 
 }
 
 void handlerCiclos( JsonObject &doc){
@@ -338,7 +356,19 @@ void comprobador(int *NuevoCiclo,int *nuevoValor){
         PublishJson();
         //Bandera
         ultimoCiclo=false;
+        //Refrescar variable para siguiente confirmación 
+        stuckConfirmador=true;
+        confirmador=false;
       }
+    
+    
+    if(millis()>=t_comprobador+20000 && stuckConfirmador){
+      if(!confirmador){
+        *NuevoCiclo=120000;
+        stuckConfirmador=false;
+        Serial.println("No se recibio confirmación, se va a valor por default");
+      }
+    }
       
     }
     
@@ -422,6 +452,8 @@ void SP_Comportamiento(int leds[],int NuevoCiclo,float relacionRojo,unsigned lon
 
   //Permitir que se pueda "enviar" de nuevo ultimoCiclo
   ultimoCiclo=true;
+
+  
   }
 }
 
